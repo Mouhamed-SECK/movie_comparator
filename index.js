ffbf99677f3eda9b87853160d8b9646b2efcee26
Mouhamed27-99 <medsoseck@gmail.com>
2020-06-22 15:16:30 +0200

    handling movie selection in autocomplete widget

--- index.js
+++ index.js
@@ -40,11 +40,15 @@ const onInput = async (e) => {
   for (let movie of movies) {
     const option = document.createElement("a");
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
     option.classList.add("dropdown-item");
-    option.innerHTML = `
-      <img src="${imgSrc}" />
-    ${movie.Title} 
-    `;
+    option.innerHTML = ` <img src="${imgSrc}" /> ${movie.Title} `;
+
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+    });
+
     resultsWrapper.appendChild(option);
   }
 };
