40df883afb720a81642f95677d422e066d4e69cd
Mouhamed27-99 <medsoseck@gmail.com>
2020-06-22 15:09:32 +0200

    adding autocomplete widget for searching

--- index.js
+++ index.js
@@ -6,13 +6,53 @@ const fetchData = async (searchTerm) => {
     },
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
 };
 
-const input = document.querySelector("input");
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label> <b>Search for a movie </b></label>
+  <input class="input"  />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
 
-const onInput = (e) => {
-  fetchData(e.target.value);
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+const onInput = async (e) => {
+  const movies = await fetchData(e.target.value);
+
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+  dropdown.classList.add("is-active");
+
+  resultsWrapper.innerHTML = "";
+  for (let movie of movies) {
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+      <img src="${imgSrc}" />
+    ${movie.Title} 
+    `;
+    resultsWrapper.appendChild(option);
+  }
 };
 
 input.addEventListener("input", debounce(onInput), 500);
+
+document.addEventListener("click", (e) => {
+  if (!root.contains(e.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
